7f785a86f3500a9bc905ec9caa7d4c11f7b36b12
LinHuaZhen <874035968@qq.com>
2020-05-22 15:15:43 +0800

    [requests]

--- HelloWorld/testdb.py
+++ HelloWorld/testdb.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from MyModel.models import AuthUser
+import requests
+
 def view(request):
     context = {}
     context['hello'] = 'hello thread'
-    data = AuthUser.objects.values()
-    print (data)
+    # data = AuthUser.objects.values()
+    r = requests.get('http://rap2.taobao.org:38080/app/mock/250909/venue-set')
+    print(r.text)
     return render(request, 'hello.html', context)
-
